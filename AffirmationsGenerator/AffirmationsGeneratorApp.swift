4cf71e2f55e0bc709d97dfa3a070ad331cc4bee4
aamilonas <angelomilonas1@gmail.com>
2025-05-05 13:28:04 -0400

    Initial Commit

---
+++ AffirmationsGenerator/AffirmationsGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmationsGeneratorApp.swift
+//  AffirmationsGenerator
+//
+//  Created by Angelo Milonas on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct AffirmationsGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AffirmationsGeneratorTests/AffirmationsGeneratorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmationsGeneratorTests.swift
+//  AffirmationsGeneratorTests
+//
+//  Created by Angelo Milonas on 5/5/25.
+//
+
+import Testing
+@testable import AffirmationsGenerator
+
+struct AffirmationsGeneratorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
